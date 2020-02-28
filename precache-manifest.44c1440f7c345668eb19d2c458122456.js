10268dc2aa5212f6bc0101dc000e488f2d921180
hollins4 <graham.hollingsworth@outlook.com>
2020-02-28 01:30:52 -0500

    Updates

---
+++ precache-manifest.44c1440f7c345668eb19d2c458122456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6fa6e22c9fcc0d1508fe8568031cb22",
+    "url": "/reactcourselist/index.html"
+  },
+  {
+    "revision": "c43473bbc99d9dc10bf7",
+    "url": "/reactcourselist/static/css/main.b1d8fadf.chunk.css"
+  },
+  {
+    "revision": "f64af3a04a3fd4ac3859",
+    "url": "/reactcourselist/static/js/2.51211c29.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/reactcourselist/static/js/2.51211c29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c43473bbc99d9dc10bf7",
+    "url": "/reactcourselist/static/js/main.2581cde7.chunk.js"
+  },
+  {
+    "revision": "f73bf420ea233ea40027",
+    "url": "/reactcourselist/static/js/runtime-main.74838644.js"
+  }
+]);
